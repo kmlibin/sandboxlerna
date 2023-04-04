df953d6b11089538c5945894644e456c0f1e3f8f
Kelli Libin <km.putnam2@gmail.com>
2023-04-04 11:49:39 -0600

    error handling

--- packages/cli/node_modules/local-api/src/index.ts
+++ packages/cli/node_modules/local-api/src/index.ts
@@ -1,3 +1,10 @@
-export const serve = (port: number, filename: string, dir: string) => {
+import express from "express";
 
+export const serve = (port: number, filename: string, dir: string) => {
+  //initial setup for express server
+  const app = express();
+  //wrapping express in our own promise, we will either resolve or reject.
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on("error", reject);
+  });
 };
--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -8,10 +8,22 @@ export const serveCommand = new Command()
   .option("-p, --port <number>", "port to run server on", "4005")
   //take in arguments - filename is optional, set the default below to notebook js
   //need to communicate the port and filename over to the local api
-  .action((filename = "notebook.js", options: { port: string }) => {
-    //how to figure out directory
-    const dir = path.join(process.cwd(), path.dirname(filename));
+  .action(async (filename = "notebook.js", options: { port: string }) => {
+    try {
+      //how to figure out directory
+      const dir = path.join(process.cwd(), path.dirname(filename));
 
-    //second arg, how to find actual filename
-    serve(parseInt(options.port), path.basename(filename), dir);
+      //second arg, how to find actual filename
+      await serve(parseInt(options.port), path.basename(filename), dir);
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file`
+      );
+    } catch (err: any) {
+      if (err.code === "EADDRINUSE") {
+        console.error("Port is already in use");
+      } else {
+        console.log("This is the problem:", err.message);
+      }
+      process.exit(1);
+    }
   });
