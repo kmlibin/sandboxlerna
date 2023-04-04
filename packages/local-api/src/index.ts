942d046dfa5d667cf171cf4915bfd2fa52ff9dd0
Kelli Libin <km.putnam2@gmail.com>
2023-04-04 12:27:28 -0600

    resolve proxy issue

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,10 +1,22 @@
 import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 export const serve = (port: number, filename: string, dir: string) => {
   //initial setup for express server
   const app = express();
+  app.use(
+    createProxyMiddleware("/", {
+        target: "http://127.0.0.1:3000/",
+        ws: true,
+        changeOrigin: true,
+        logLevel: "silent",
+        headers: {
+          Connection: "keep-alive",
+        },
+      })
+  );
   //wrapping express in our own promise, we will either resolve or reject.
   return new Promise<void>((resolve, reject) => {
-    app.listen(port, resolve).on("error", reject);
+    app.listen(port, "0.0.0.0", resolve).on("error", reject);
   });
 };
