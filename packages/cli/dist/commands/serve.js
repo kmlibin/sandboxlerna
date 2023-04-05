40e4e7320ab9c13801eae71525bb7d294b5828d3
Kelli Libin <km.putnam2@gmail.com>
2023-04-05 10:14:38 -0600

    add local client

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,15 +16,28 @@ exports.serveCommand = void 0;
 const path_1 = __importDefault(require("path"));
 const local_api_1 = require("local-api");
 const commander_1 = require("commander");
+const isProduction = process.env.NODE_ENV === "production";
 exports.serveCommand = new commander_1.Command()
     .command("serve [filename]")
     .description("Open a file for editing")
     .option("-p, --port <number>", "port to run server on", "4005")
     //take in arguments - filename is optional, set the default below to notebook js
     //need to communicate the port and filename over to the local api
-    .action((filename = "notebook.js", options) => {
-    //how to figure out directory
-    const dir = path_1.default.join(process.cwd(), path_1.default.dirname(filename));
-    //second arg, how to find actual filename
-    (0, local_api_1.serve)(parseInt(options.port), path_1.default.basename(filename), dir);
-});
+    .action((filename = "notebook.js", options) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        //how to figure out directory
+        const dir = path_1.default.join(process.cwd(), path_1.default.dirname(filename));
+        //second arg, how to find actual filename
+        yield (0, local_api_1.serve)(parseInt(options.port), path_1.default.basename(filename), dir, !isProduction);
+        console.log(`Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file`);
+    }
+    catch (err) {
+        if (err.code === "EADDRINUSE") {
+            console.error("Port is already in use");
+        }
+        else {
+            console.log("This is the problem:", err.message);
+        }
+        process.exit(1);
+    }
+}));
--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,6 +1,39 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
-const serve = (port, filename, dir) => {
+const express_1 = __importDefault(require("express"));
+const http_proxy_middleware_1 = require("http-proxy-middleware");
+const path_1 = __importDefault(require("path"));
+const cells_1 = require("./routes/cells");
+//running locally on our machine, then we want a proxy
+//running on a users machine, serve up files from the build directory in local-client
+const serve = (port, filename, dir, useProxy) => {
+    //initial setup for express server
+    const app = (0, express_1.default)();
+    if (useProxy) {
+        app.use((0, http_proxy_middleware_1.createProxyMiddleware)("/", {
+            target: "http://127.0.0.1:3000/",
+            ws: true,
+            changeOrigin: true,
+            logLevel: "silent",
+            headers: {
+                Connection: "keep-alive",
+            },
+        }));
+    }
+    else {
+        //figures out the location of index.html. will be the absolute path, not relative
+        const packagePath = require.resolve("local-client/build/index.html");
+        //don't want the entire path, just everything up to the build folder
+        app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    }
+    app.use((0, cells_1.createCellsRouter)(filename, dir));
+    //wrapping express in our own promise, we will either resolve or reject.
+    return new Promise((resolve, reject) => {
+        app.listen(port, "0.0.0.0", resolve).on("error", reject);
+    });
 };
 exports.serve = serve;
